6cf3eb77858c5da29a6d5704dd780ea3ffca8bd5
Yuriy Kitsul <yuriyk@nayax.com>
2024-12-19 17:25:02 -0500

    add error handling examples

--- data-types/integer.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract IntegersExample {
-    int256 public signedInt = -100; // Signed integer
-    uint256 public unsignedInt = 100; // Unsigned integer
-}
